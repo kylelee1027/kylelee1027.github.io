89167a607b34ad868e94af4d183adbdbd648acd8
kylelee1027 <kylelee1027@gmail.com>
2024-07-04 17:18:53 -0400

    aa

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/kylelee1027.github.io/"
+  plugins: [react()]
 })
